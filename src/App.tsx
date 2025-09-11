5f63a55f264fa8e837a25042689e04c54fcd24b2
earamburo <edwin.aramburo1@gmail.com>
2025-09-11 16:03:59 -0400

    change name

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter  basename="/crisp-creative-craft">
+      <BrowserRouter  basename="/AutoRama">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
